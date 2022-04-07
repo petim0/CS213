857a66c17f4a407bc9695f3961d2c649e5cbc345
Timo Achard <timo@apolline.eu>
2022-04-07 15:27:29 +0200

    Added a button 'back'

--- Assets/Scripts/Game/scenManager.cs
+++ Assets/Scripts/Game/scenManager.cs
@@ -8,12 +8,16 @@ public class scenManager : MonoBehaviour
 
     public Text player1Text;
     public Text player2Text;
+    public Text time;
+    private float ftime = 0;
+
 
     // Start is called before the first frame update
     void Start()
     {
         player1Text.text = "Player 1: " + PersistentManagerScript.Instance.player1Score.ToString();
         player2Text.text = "Player 2: " + PersistentManagerScript.Instance.player2Score.ToString();
+        time.text = "00:00";
 
     }
 
@@ -22,5 +26,10 @@ public class scenManager : MonoBehaviour
     {
         player1Text.text = "Player 1: " + PersistentManagerScript.Instance.player1Score.ToString();
         player2Text.text = "Player 2: " + PersistentManagerScript.Instance.player2Score.ToString();
+
+        ftime += Time.deltaTime;
+        float minutes = Mathf.FloorToInt(ftime / 60);
+        float seconds = Mathf.FloorToInt(ftime % 60);
+        time.text = string.Format("{0:00}:{1:00}", minutes, seconds);
     }
 }
