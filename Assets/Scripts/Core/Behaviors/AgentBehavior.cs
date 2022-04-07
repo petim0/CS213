b0262030b57ade9d5264398727c630cbd017cdad
Navaranjan Arun <arun.navaranjan@epfl.ch>
2022-04-07 12:09:08 +0200

    Implementation 4.2 step 2 en cours

--- Assets/Scripts/Core/Behaviors/AgentBehavior.cs
+++ Assets/Scripts/Core/Behaviors/AgentBehavior.cs
@@ -1,9 +1,28 @@
 using UnityEngine;
-using System.Collections;
+
 public class AgentBehaviour : MonoBehaviour
 {
     public float weight = 1.0f;
     protected CelluloAgent agent;
+
+    public GameObject[] sheeps;
+    public GameObject[] players;
+
+    const int minNumberPlayer = 2;
+
+
+    public void Start()
+    {
+        //Set the tag of this GameObject to Player
+        gameObject.tag = "Player";
+        sheeps = GameObject.FindGameObjectsWithTag("Sheep");
+        players = GameObject.FindGameObjectsWithTag("Player");
+        
+        if (players.Length < minNumberPlayer) {
+            Debug.Log("Not enough Players to start the Game, don't you have friends?");
+        }
+    }
+
 
     public virtual void Awake()
     {
--- Assets/Scripts/Core/Behaviors/GhostSheepBehavior.cs
+++ Assets/Scripts/Core/Behaviors/GhostSheepBehavior.cs
@@ -1,18 +1,59 @@
-﻿using System.Linq;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GhostSheepBehavior : AgentBehaviour
 {    
-    public void Start(){
-    }
+    const string ennemiesTag = "Dog";
+    const int detectionRadius = 50;
+
     public override Steering GetSteering()
     {
         
         Steering steering = new Steering();
         //implement your code here.
+        GameObject[] ennemies;
+        ennemies = GameObject.FindGameObjectsWithTag(ennemiesTag);
+        //Check to see if the tag on the collider is equal to Enemy
+        bool ennemyIsClose = false;
+        Vector3[] ennemyDirection;
+        int i = 0;
+        foreach (GameObject ennemy in ennemies)
+        {
+
+            //Get distance
+            Vector3 diff = ennemy.transform.position - this.transform.position;
+
+            // Get direction from A to B
+            ennemyDirection[i] = (-diff).normalized; // from go.position to position
+            i +=1 ;
+
+            float curDistance = diff.sqrMagnitude;
+            if (curDistance < detectionRadius)
+            {
+                ennemyIsClose = true;
+            }
+        }
+
+        Vector3 newDir = new Vector3(0.0f, 0.0f, 0.0f);
+        if (ennemyIsClose) {
+            foreach (Vector3 dir in ennemyDirection)
+            {
+                newDir = newDir + dir;
+            }
+            newDir = newDir.normalized;
+        }
+
         return steering;
     }
 
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Check to see if the tag on the collider is equal to Enemy
+        if (other.tag == ennemiesTag)
+        {
+            Debug.Log("Triggered by Enemy");
+        }
+    }
 
 
 }
