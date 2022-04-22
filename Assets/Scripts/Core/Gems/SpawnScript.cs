f66a141c5324637b912822563c206b96a7d6cdd1
Alexis Firome <alexis.firome@epfl.ch>
2022-04-22 10:23:22 +0200

    sound effect for gem

--- Assets/Scripts/Core/Gems/SpawnScript.cs
+++ Assets/Scripts/Core/Gems/SpawnScript.cs
@@ -6,12 +6,11 @@ public class SpawnScript : MonoBehaviour
 {
     public GameObject spawnzone;
     public GameObject[] gems;
-    public float spawnTime = 50f;        
-    public float spawnDelay = 3f;
+    public float spawnTime;        
+    public float spawnDelay;
     public AudioClip audioSpawn;
-    AudioSource audio;
+    AudioSource aud;
 
-    // Start is called before the first frame update
     void Start()
     {
         InvokeRepeating("Spawn", spawnDelay, spawnTime);        
@@ -19,8 +18,8 @@ public class SpawnScript : MonoBehaviour
 
     void Spawn()
     {
-        audio = GetComponent<AudioSource>();
-        audio.PlayOneShot(audioSpawn, 0.7F);
+        aud = GetComponent<AudioSource>();
+        aud.PlayOneShot(audioSpawn, 0.7F);
         int gemIndex = Random.Range(0, gems.Length);
         float x = Random.Range(spawnzone.GetComponent<BoxCollider>().bounds.min.x + 2, spawnzone.GetComponent<BoxCollider>().bounds.max.x - 2);
         float z = Random.Range(spawnzone.GetComponent<BoxCollider>().bounds.min.z + 2, spawnzone.GetComponent<BoxCollider>().bounds.max.z - 2);
@@ -28,10 +27,6 @@ public class SpawnScript : MonoBehaviour
         Instantiate(gems[gemIndex], position , transform.rotation);
     }
 
-    // Update is called once per frame 
-    void Update()
-    {
-    }
 } 
  
 
