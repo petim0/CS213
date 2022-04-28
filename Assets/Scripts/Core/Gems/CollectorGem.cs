429b3ecbca5595b4684f4a93df2f3ed7dad26571
Alexis Firome <alexis.firome@epfl.ch>
2022-04-28 10:52:34 +0200

    end of step 3 missing audio

--- Assets/Scripts/Core/Gems/CollectorGem.cs
+++ Assets/Scripts/Core/Gems/CollectorGem.cs
@@ -4,36 +4,28 @@ using UnityEngine;
 
 public class CollectorGem : MonoBehaviour
 {
-    public GameObject gameobject;
-    //CollectorPlayer player1;
+    CollectorPlayer player1;
     CollectorPlayer player2;
-    public AudioClip audioCollected;
-    AudioSource aud;
+    public AudioSource aud;
 
-    /*private void Awake()
-    {
-        player1 = gameObject.GameObject.Find("Player1").GetComponent<CollectorPlayer>();
-        player2 = gameObject.GameObject.Find("Player2").GetComponent<CollectorPlayer>();
-    }*/
+
 
     public void OnTriggerEnter(Collider other)
     {
 
         if (other.gameObject.transform.tag.CompareTo("Player1") == 1)
         {
-            
+            player1 = other.gameObject.GetComponentInParent<CollectorPlayer>(); 
+            aud.Play();
             gameObject.SetActive(false);
-            aud = GetComponent<AudioSource>();
-            aud.PlayOneShot(audioCollected, 0.7F);
-            //player1.DiamondCollected();
+            player1.DiamondCollected();
 
         }
         else if(other.gameObject.transform.tag.CompareTo("Player2") == 1)
         {
+            other.gameObject.GetComponentInParent<CollectorPlayer>().DiamondCollected(); ;
+            aud.Play();
             gameObject.SetActive(false);
-            aud = GetComponent<AudioSource>();
-            aud.PlayOneShot(audioCollected, 0.7F);
-            //player2.DiamondCollected();
 
 
         }
